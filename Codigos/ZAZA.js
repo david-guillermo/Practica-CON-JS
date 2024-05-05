46a93ab0a0ced575236f908688a4bf564d84dcd7
david-guillermo <davicraft.yt15@gmail.com>
2024-05-05 19:39:55 -0400

    jhv

---
+++ Codigos/ZAZA.js
@@ -0,0 +1,11 @@
+const option = +prompt("Selecionas uno: \n1. The last of us \n2. Red dead redemption \n3. Maincraft \n4. GTA V"); 
+
+if(opcion === "1"){
+    alert("JUegazo (❤´艸｀❤)");
+}else if(opcion === "2"){
+    alert("A perro si le sabes a gaming ☜(ﾟヮﾟ☜)")
+}else if(opcion === "3"){
+    alert("TECNICO O AVENTURERO?")
+}else if(opcion === "4"){
+    alert("DEJA EL VIZIO COMPA")
+}
